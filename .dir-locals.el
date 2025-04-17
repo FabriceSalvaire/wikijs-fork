487596fb1975353f2e2ce382d183aad82a316a6e
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2025-04-17 22:14:41 +0200

    Emacs .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+
+;; js2-basic-offset
+
+((nil . ((tab-width . 2)
+         (js-indent-level . 2)))
+
+ (js-mode . ((tab-width . 2)
+             (js-indent-level . 2)))
+
+ (html-mode . ((tab-width . 2))))
