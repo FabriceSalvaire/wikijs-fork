35a1bd81947ac24ee672cff21eeeb0515fc2d59e
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2025-04-19 18:09:04 +0200

    github tool

--- tools/github-tool.py
+++ tools/github-tool.py
@@ -11,6 +11,10 @@ import requests
 
 ####################################################################################################
 
+ROOT_SOURCE = Path(__file__).parents[1]
+PR_DIR = ROOT_SOURCE.joinpath('pr')
+PULLS_JSON_FILE = PR_DIR.joinpath('pulls.json')
+
 REPOSITORY_NAME = 'requarks/wiki'
 
 ####################################################################################################
@@ -50,36 +54,68 @@ def get_repo():
 
 ####################################################################################################
 
-root_source = Path(__file__).parents[1]
-pr_dir = root_source.joinpath('pr')
+def cleanup_dict(data: dict, keys: list[str]) -> dict:
+    return {key: value for key, value in data.items() if key in keys}
+
+def cleanup_user(data: dict) -> dict:
+    return cleanup_dict(data, ('login', 'id'))
+
+def cleanup_repo(data: dict) -> dict:
+    return cleanup_dict(data, ('url', 'id'))
+
+def cleanup_pulls(data: dict) -> None:
+    for key, value in data.items():
+        match key:
+            case 'user' | 'assignee' | 'owner':
+                if isinstance(value, dict):
+                    data[key] = cleanup_user(value)
+            case 'assignees' | 'requested_reviewers':
+                data[key] = [cleanup_user(_) for _ in value]
+            case 'repo':
+                data[key] = cleanup_repo(value)
+        match value:
+            case dict():
+                cleanup_pulls(value)
+
+####################################################################################################
+
+def dump_pull_request(dump_json: bool = True) -> None:
+    pulls_json = {}
+
+    pulls = repo.get_pulls(state='open', sort='created', base='main')
+    for pr in pulls:
+        time.sleep(1)
+        print(pr)
+        # pprint(pr.__dict__)
+        # pprint(pr._rawData)
+        # pulls_json[pr.number] = {
+        #     'title': pr.title,
+        # }
+        # raw data contains a lot of noises !
+        pulls_json[pr.number] = pr._rawData
+        pulls_json[pr.number]['comments'] = [_._rawData for _ in pr.get_comments()]
+        # Write Diff
+        diff_file = PR_DIR.joinpath(f'pr-{pr.number}.diff')
+        if not diff_file.exists():
+            # [Get pull request diff from API](https://github.com/orgs/community/discussions/24460)
+            # f'https://github.com/{REPOSITORY_NAME}/pull/{pr.number}.diff'
+            rq = requests.get(pr.diff_url)
+            diff_file.write_text(rq.text)
+
+    if dump_json:
+        cleanup_pulls(pulls_json)
+        with open(PULLS_JSON_FILE, 'w', encoding='utf8') as fh:
+            json.dump(
+                pulls_json,
+                fh,
+                sort_keys=True,
+                indent=4,
+            )
+
+####################################################################################################
 
 repo = get_repo()
-
-pulls_json = {}
-pulls = repo.get_pulls(state='open', sort='created', base='main')
-for pr in pulls:
-    print(pr)
-    # pprint(pr.__dict__)
-    # pprint(pr._rawData)
-    # pulls_json[pr.number] = {
-    #     'title': pr.title,
-    # }
-    pulls_json[pr.number] = pr._rawData
-    diff_file = pr_dir.joinpath(f'pr-{pr.number}.diff')
-    if not diff_file.exists():
-        # [Get pull request diff from API](https://github.com/orgs/community/discussions/24460)
-        # f'https://github.com/{REPOSITORY_NAME}/pull/{pr.number}.diff'
-        rq = requests.get(pr.diff_url)
-        diff_file.write_text(rq.text)
-
-JSON_FILE = pr_dir.joinpath('pulls.json')
-with open(JSON_FILE, 'w', encoding='utf8') as fh:
-    json.dump(
-        pulls_json,
-        fh,
-        sort_keys=True,
-        indent=4,
-    )
+dump_pull_request()
 
 # issues = repo.get_issues()
 # for _ in issues:
@@ -87,3 +123,6 @@ with open(JSON_FILE, 'w', encoding='utf8') as fh:
 #     print(list(_.get_labels()))
 #     print(list(_.get_comments()))
 #     time.sleep(10)
+
+# with open(PULLS_JSON_FILE, 'r', encoding='utf8') as fh:
+#     pulls_json = json.load(fh)
