cf825d88abc5930059d21a753741168f59dcb3f1
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2025-04-24 15:35:59 +0200

    tool

--- tools/rebuild.sh
+++ tools/rebuild.sh
@@ -4,7 +4,5 @@ rm node_modules
 ln -sf /srv/cache/fabrice/node_modules-prod node_modules
 yarn build
 
-./tools/clean-assets.sh
-
 rm node_modules
 ln -sf /srv/cache/fabrice/node_modules node_modules
