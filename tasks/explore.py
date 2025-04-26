6116d8d8eeab09c61764e2c6d811a26025098d19
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2025-04-26 22:53:44 +0200

    task explore

---
+++ tasks/explore.py
@@ -0,0 +1,346 @@
+#! /usr/bin/env python3
+
+"""This module implements a JS/Vue source code explorer.
+
+It features
+- A tool to explore imports in the source code and match them with `package.json` dependencies.
+- A tool to upgrade `@mdi/font` package aka MDI aka Material Design Icons:
+  lookup for mdi icons in the source code and check for upgrade using the changelog.
+"""
+
+####################################################################################################
+
+from pprint import pprint
+from typing import Iterator
+from pathlib import Path
+import json
+
+from invoke import task
+
+from .helper import printc
+import build
+
+####################################################################################################
+
+SOURCE_PATH = Path(__file__).parents[1]
+
+####################################################################################################
+
+def yield_source_files(source_path: Path, suffixes: list[str] = ('.js', '.vue')) -> Iterator[Path]:
+    for dir in ('client', 'server'):
+        for root, dirs, filenames in source_path.joinpath(dir).walk():
+            for _ in filenames:
+                path = Path(root) / _
+                if suffixes is None or path.suffix in suffixes:
+                    yield path
+
+####################################################################################################
+
+def yield_imports(source_path: Path) -> Iterator[tuple[Path, Path, str]]:
+    for path in yield_source_files(source_path):
+        yield from yield_file_imports(source_path, path)
+
+####################################################################################################
+
+def yield_file_imports(source_path: Path, path: Path) -> Iterator[tuple[Path, Path, str]]:
+    # print(path)
+    local_dirs = [_.name for _ in path.parent.iterdir() if _.is_dir()]
+    # if dirs:
+    #     print(dirs)
+    with open(path, 'r', encoding='utf8') as fh:
+        for line in fh:
+            line = oline = line.strip()
+            # strip comment
+            _ = line.rfind('//')
+            if _ != -1:
+                line = line[:_].rstrip()
+            module = None
+            if line.startswith('import '):
+                if 'from' in line:
+                    # import ... from module
+                    _ = line.find('from ')
+                    if _ == -1:
+                        raise ValueError(line)
+                    module = line[_+5:]
+                else:
+                    # import module
+                    _ = line.find(' ')
+                    if _ == -1:
+                        raise ValueError(line)
+                    module = line[_+1:].strip()
+            else:
+                # Node.js import
+                _ = line.find('require(')
+                if _ != -1:
+                    module = line[_+8:]
+                    # _ = module.find(')')
+                    # if _ == -1:
+                    #     raise ValueError(line)
+                    level = 1
+                    for i, c in enumerate(module):
+                        match c:
+                            case '(':
+                                level += 1
+                            case ')':
+                                level -= 1
+                        if not level:
+                            break
+                    module = module[:i]
+            if module is not None:
+                if module.startswith("'"):
+                    module = module[1:-1]
+                else:
+                    module = module
+                # print(oline)
+                # print(f'  {module}')
+                module = Path(module)
+                type_ = 'external'
+                first = module.parts[0]
+                # print(first)
+                if first in ('.', '..') or module.suffix in ('.vue',):
+                    module = path.parent.joinpath(module).resolve().relative_to(source_path)
+                    type_ = 'internal'
+                elif first in ('gql',):
+                    module = source_path.joinpath('client', 'graph').joinpath(module).relative_to(source_path)
+                    type_ = 'internal'
+                # if first in local_dirs or first in ('gql',):
+                #     module = source_path.joinpath('client', 'graph').joinpath(module).relative_to(source_path)
+                #     type_ = 'internal'
+                else:
+                    def check_exists(suffix: str = '') -> bool:
+                        nonlocal module
+                        nonlocal type_
+                        _ = str(module)
+                        if suffix and not _.endswith(suffix):
+                            _ += '.js'
+                        _ = path.parent.joinpath(_)
+                        if _.exists():
+                            module = _.relative_to(source_path)
+                            type_ = 'internal'
+                    if not check_exists('.js'):
+                        check_exists()
+                    # Fixme: client/libs/modernizr/modernizr.js
+                    # else server/modules/logging
+                module_ = str(module)
+                if '{' in module_ or '(' in module_:
+                    type_ = 'complex'
+                    # print(path)
+                    # print(' '*4 + oline)
+                    # print(' '*4 + module_)
+                yield (path.relative_to(source_path), module, type_)
+
+####################################################################################################
+
+@task
+def dump_imports(ctx, json_file: Path) -> None:
+    complex_imports = {}
+    external_imports = {}
+    internal_imports = {}
+    for path, module, type_ in yield_imports(SOURCE_PATH):
+        # print(f'{path}  ->  {module}   {is_external}')
+        module_ = str(module)
+        match type_:
+            case 'complex':
+                imports = complex_imports
+            case 'external':
+                imports = external_imports
+            case 'internal':
+                imports = internal_imports
+        imports.setdefault(module_, set())
+        imports[module_].add(str(path))
+
+    # def print_imports(imports: dict) -> None:
+    #     for module in sorted(imports.keys()):
+    #         print(module)
+    #         for _ in imports[module]:
+    #             print(' '*4 + _)
+    # print_imports(imports)
+
+    def custom_json(obj):
+        if isinstance(obj, set):
+            return sorted(obj)
+        raise TypeError(f'Cannot serialize object of {type(obj)}')
+
+    imports = {
+        'complex': complex_imports,
+        'external': external_imports,
+        'internal': internal_imports,
+    }
+    printc(f'<red>Write {json_file}</red>')
+    json_file.write_text(
+        json.dumps(
+            imports,
+            default=custom_json,
+            sort_keys=True,
+            indent=4,
+        )
+    )
+    return imports
+
+####################################################################################################
+
+@task(build.symlink_dev)
+def explore_dependencies(ctx):
+    imports_json_file = Path('imports.json')
+    imports = dump_imports(ctx, imports_json_file)
+    # imports = json.loads(imports_json_file.read_text())
+
+    package_json_file = SOURCE_PATH.joinpath('package.json')
+    package_json = json.loads(package_json_file.read_text())
+    dependencies = package_json['dependencies']
+    dev_dependencies = package_json['devDependencies']
+
+    external_imports = imports['external']
+    print()
+    def check_dependencies(type_, dependencies):
+        printc(f'<blue>{type_}Dependency not imported:</blue>')
+        for dependency in sorted(dependencies):
+            if dependency not in external_imports:
+                print(' '*2 + dependency)
+    check_dependencies('', dependencies)
+    # check_dependencies('dev', dev_dependencies)
+
+    node_modules_path = SOURCE_PATH.joinpath('node_modules')
+    # node_modules = [_.name for _ in node_modules_path.iterdir()]
+    NODE_LIBS = (
+        'crypto',
+        'fs',
+        'http',
+        'https',
+        'os',
+        'path',
+        'stream',
+        'url',
+        'util',
+        'zlib',
+    )
+
+    print()
+    printc('<blue>Import not found:</blue>')
+    external_imports_keys = sorted(external_imports.keys())
+    for dependency in external_imports_keys:
+        files = external_imports[dependency]
+        # files.sort()
+        if dependency not in NODE_LIBS and not node_modules_path.joinpath(dependency).exists():
+            printc(' '*2 + f'<green>{dependency}</green>')
+            for _ in sorted(files):
+                print(' '*6 + _)
+
+    print()
+    printc('<blue>Packages:</blue>')
+    for dependency in external_imports_keys:
+        files = external_imports[dependency]
+        if dependency not in NODE_LIBS and node_modules_path.joinpath(dependency).exists():
+            printc(' '*2 + f'<green>{dependency}</green>')
+            for _ in sorted(files):
+                print(' '*6 + _)
+
+    print()
+    printc('<blue>Packages:</blue>')
+    map = {
+        'server': [],
+        'client': [],
+    }
+    for dependency, files in external_imports.items():
+        if dependency not in NODE_LIBS and node_modules_path.joinpath(dependency).exists():
+            for _ in set([Path(_).parts[0] for _ in files]):
+                map[_].append(dependency)
+
+    def find_depency(_):
+        type_ = ''
+        version = '???'
+        if _ in dependencies:
+            version = dependencies[_]
+        elif _ in dev_dependencies:
+            type_ = 'dev'
+            version = dev_dependencies[_]
+        return type_, version
+
+    for key, values in map.items():
+        print()
+        printc(f'<green>{key}</green>')
+        lines = []
+        dev_lines = []
+        for package in sorted(values):
+            type_, version = find_depency(package)
+            if version == '???':
+                type_, version = find_depency(Path(package).parts[0])
+            line = f'"{package}": "<blue>{version}</blue>";'
+            if type_:
+                _ = dev_lines
+            else:
+                _ = lines
+            _.append(line)
+
+        def print_lines(lines):
+            for _ in lines:
+                printc(' '*2 + _)
+
+        print_lines(lines)
+        printc('<red>dev</red>')
+        print_lines(dev_lines)
+
+####################################################################################################
+
+@task
+def lookup_mdi(ctx):
+    """Check MDI Icons"""
+    icon_names = set()
+    for path in yield_source_files(SOURCE_PATH, ('.vue',)):
+        with open(path, 'r', encoding='utf8') as fh:
+            for line in fh:
+                line = line.strip()
+                # strip comment
+                _ = line.rfind('//')
+                if _ != -1:
+                    line = line[:_].rstrip()
+                MDI = 'mdi-'
+                _ = line.find(MDI)
+                if _ != -1:
+                    # print(line)
+                    left = line[_+4:]
+                    name = MDI
+                    complex = False
+                    for c in left:
+                        if c == '{':
+                            complex = True
+                        elif complex:
+                            if c == '}':
+                                break
+                        else:
+                            if c in (' \'"]'):
+                                break
+                        name += c
+                    name = name[len(MDI):]
+                    icon_names.add(name)
+
+    from mdi import MDI_CHANGES
+    # pprint(MDI_CHANGES)
+    print('To be fixed:')
+    for _ in sorted(icon_names):
+        # print(_)
+        if _ in MDI_CHANGES:
+            print(f'  {_} {MDI_CHANGES[_]}')
+        elif '{' in _:
+            print(f'  ??? {_}')
+
+####################################################################################################
+
+@task
+def dump_file_tree(ctx) -> None:
+    # ['.asar', '.css', '.gql', '.graphql', '.html', '.ico', '.jpg', '.js', '.json', '.md',
+    #  '.png', '.pug', '.scss', '.svg', '.vue', '.woff', '.woff2', '.xml', '.yml']
+    suffixes = set()
+    for dir in ('client', 'server'):
+        for root, dirs, filenames in SOURCE_PATH.joinpath(dir).walk():
+            dirs.sort()
+            root = root.relative_to(SOURCE_PATH)
+            INDENT = ' '*4
+            indentation = INDENT*(len(root.parts)-1)
+            print(indentation + root.name)
+            for _ in sorted(filenames):
+                suffix = Path(_).suffix
+                suffixes.add(suffix)
+                if suffix in ('.js', '.vue'):
+                    print(indentation + INDENT + _)
+    print(sorted(suffixes))
