253a627a485be223197fc8b0e4dd43eae7e601c7
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2025-05-21 02:42:00 +0200

    reformat express-request-logger

--- wikijs-server/server/forked-libraries/express-request-logger/index.js
+++ wikijs-server/server/forked-libraries/express-request-logger/index.js
@@ -1,54 +1,52 @@
-'use strict';
+'use strict'
 
 var _ = require('lodash'),
     logger = require('bunyan').createLogger({ name: 'ExpressLogger' }),
     loggerHelper = require('./logger-helper'),
     setupOptions,
-    flatten = require('flat');
+    flatten = require('flat')
 
 var audit = function (req, res, next) {
-    var oldWrite = res.write;
-    var oldEnd = res.end;
-    var oldJson = res.json;
-    var chunks = [];
+    var oldWrite = res.write
+    var oldEnd = res.end
+    var oldJson = res.json
+    var chunks = []
 
     // Log start time of request processing
-    req.timestamp = new Date();
+    req.timestamp = new Date()
 
-    if (setupOptions.doubleAudit) {
-        loggerHelper.auditRequest(req, setupOptions);
-    }
+    if (setupOptions.doubleAudit)
+        loggerHelper.auditRequest(req, setupOptions)
 
     res.write = function (chunk) {
-        chunks.push(Buffer.from(chunk));
-        oldWrite.apply(res, arguments);
-    };
+        chunks.push(Buffer.from(chunk))
+        oldWrite.apply(res, arguments)
+    }
 
     // decorate response#json method from express
     res.json = function (bodyJson) {
-        res._bodyJson = bodyJson;
-        oldJson.apply(res, arguments);
-    };
+        res._bodyJson = bodyJson
+        oldJson.apply(res, arguments)
+    }
 
     // decorate response#end method from express
     res.end = function (chunk) {
-        res.timestamp = new Date();
-        if (chunk) {
-            chunks.push(Buffer.from(chunk));
-        }
+        res.timestamp = new Date()
+        if (chunk)
+            chunks.push(Buffer.from(chunk))
 
-        res._bodyStr = Buffer.concat(chunks).toString('utf8');
+        res._bodyStr = Buffer.concat(chunks).toString('utf8')
 
         // call to original express#res.end()
-        oldEnd.apply(res, arguments);
-        loggerHelper.auditResponse(req, res, setupOptions);
-    };
+        oldEnd.apply(res, arguments)
+        loggerHelper.auditResponse(req, res, setupOptions)
+    }
 
-    next();
-};
+    next()
+}
 
 module.exports = function (options) {
-    options = options || {};
+    options = options || {}
     var defaults = {
         logger: logger,
         request: {
@@ -74,37 +72,39 @@ module.exports = function (options) {
             '4xx': 'info',
             '5xx': 'error'
         },
-        shouldSkipAuditFunc: function(req, res){
-            return false;
+        shouldSkipAuditFunc: function (req, res) {
+            return false
         }
-    };
+    }
 
-    _.defaultsDeep(options, defaults);
-    setupOptions = validateArrayFields(options, defaults);
-    setBodyLengthFields(setupOptions);
-    return audit;
-};
+    _.defaultsDeep(options, defaults)
+    setupOptions = validateArrayFields(options, defaults)
+    setBodyLengthFields(setupOptions)
+    return audit
+}
 
 // Convert all options fields that need to be array by default
 function validateArrayFields(options, defaults) {
-    let defaultsCopy = Object.assign({}, defaults);
-    delete defaultsCopy.logger;
+    let defaultsCopy = Object.assign({}, defaults)
+    delete defaultsCopy.logger
 
     Object.keys(flatten(defaultsCopy)).forEach((key) => {
-        let optionValue = _.get(options, key);
+        let optionValue = _.get(options, key)
         let defaultValue = _.get(defaultsCopy, key)
         if (_.isArray(defaultValue) && !_.isArray(optionValue)) {
             // throw error - wrong type passed
-            let errMsg = `Invalid value specified for field: ${key}, expected array`;
-            throw new Error(errMsg);
+            let errMsg = `Invalid value specified for field: ${key}, expected array`
+            throw new Error(errMsg)
         }
-    });
+    })
 
-    return options;
+    return options
 }
 
 function setBodyLengthFields(options) {
-    const isValid = field => field && !isNaN(field) && field > 0;
-    options.request.maxBodyLength = !isValid(options.request.maxBodyLength) ? undefined : options.request.maxBodyLength;
-    options.response.maxBodyLength = !isValid(options.response.maxBodyLength) ? undefined : options.response.maxBodyLength;
+    const isValid = (field) => field && !isNaN(field) && field > 0
+    options.request.maxBodyLength = !isValid(options.request.maxBodyLength) ? undefined : options.request.maxBodyLength
+    options.response.maxBodyLength = !isValid(options.response.maxBodyLength) ?
+        undefined :
+        options.response.maxBodyLength
 }
--- wikijs-server/server/forked-libraries/express-request-logger/utils.js
+++ wikijs-server/server/forked-libraries/express-request-logger/utils.js
@@ -1,89 +1,85 @@
-'use strict';
+'use strict'
 
-var _ = require('lodash');
-const MASK = 'XXXXX';
-const NA = 'N/A';
+var _ = require('lodash')
+const MASK = 'XXXXX'
+const NA = 'N/A'
 const VALID_LEVELS = ['trace', 'debug', 'info', 'warn', 'error']
-const DEFAULT_LEVEL = 'info';
+const DEFAULT_LEVEL = 'info'
 
 var getUrl = function (req) {
-    var url = req && req.url || NA;
+    var url = req && req.url || NA
 
-    return url;
-};
+    return url
+}
 
 var getRoute = function (req) {
-    var url = NA;
+    var url = NA
 
     if (req) {
-        var route = req.baseUrl;
-        if (req.route && route) {
-            url = route + req.route.path;
-        } else if (req.route) {
-            url = req.route.path;
-        }
+        var route = req.baseUrl
+        if (req.route && route)
+            url = route + req.route.path
+        else if (req.route)
+            url = req.route.path
     }
 
-    return url;
-};
+    return url
+}
 
 function cleanOmitKeys(obj, omitKeys) {
     if (obj && !_.isEmpty(omitKeys)) {
         Object.keys(obj).forEach(function (key) {
-            if (_.some(omitKeys, omitKey => key === omitKey)) {
-                delete obj[key];
-            } else {
-                (obj[key] && typeof obj[key] === 'object') && cleanOmitKeys(obj[key]);
-            }
-        });
+            if (_.some(omitKeys, (omitKey) => key === omitKey))
+                delete obj[key]
+            else
+                (obj[key] && typeof obj[key] === 'object') && cleanOmitKeys(obj[key])
+        })
     }
-    return obj;
-};
+    return obj
+}
 
 var shouldAuditURL = function (excludeURLs, req) {
     return _.every(excludeURLs, function (path) {
-        var url = getUrl(req);
-        var route = getRoute(req);
-        return !(url.includes(path) || route.includes(path));
-    });
-};
+        var url = getUrl(req)
+        var route = getRoute(req)
+        return !(url.includes(path) || route.includes(path))
+    })
+}
 
 var maskJson = function (jsonObj, fieldsToMask) {
     let jsonObjCopy = _.cloneDeepWith(jsonObj, function (value, key) {
-        if (_.includes(fieldsToMask, key)) {
+        if (_.includes(fieldsToMask, key))
             return MASK
-        }
     })
-    return jsonObjCopy;
-};
+    return jsonObjCopy
+}
 
 var getLogLevel = function (statusCode, levelsMap) {
-    let level = DEFAULT_LEVEL; // Default
+    let level = DEFAULT_LEVEL // Default
 
     if (levelsMap) {
-        let status = statusCode.toString();
+        let status = statusCode.toString()
 
-        if (levelsMap[status]) {
-            level = VALID_LEVELS.includes(levelsMap[status]) ? levelsMap[status] : level;
-        } else {
-            let statusGroup = `${status.substring(0, 1)}xx`; // 5xx, 4xx, 2xx, etc..
-            level = VALID_LEVELS.includes(levelsMap[statusGroup]) ? levelsMap[statusGroup] : level;
+        if (levelsMap[status])
+            level = VALID_LEVELS.includes(levelsMap[status]) ? levelsMap[status] : level
+        else {
+            let statusGroup = `${status.substring(0, 1)}xx` // 5xx, 4xx, 2xx, etc..
+            level = VALID_LEVELS.includes(levelsMap[statusGroup]) ? levelsMap[statusGroup] : level
         }
     }
 
-    return level;
+    return level
 }
 
 var getBodyStr = function (body, maxBodyLength) {
-    if (_.isEmpty(body)) {
+    if (_.isEmpty(body))
         return NA
-    } else {
-        let bodyStr = (typeof body !== 'string') ? JSON.stringify(body) : body;
-        let shouldShorten = maxBodyLength && maxBodyLength > 0 && bodyStr.length > maxBodyLength;
-        return shouldShorten ? bodyStr.substr(0, maxBodyLength) + '...' : bodyStr;
+    else {
+        let bodyStr = (typeof body !== 'string') ? JSON.stringify(body) : body
+        let shouldShorten = maxBodyLength && maxBodyLength > 0 && bodyStr.length > maxBodyLength
+        return shouldShorten ? bodyStr.substr(0, maxBodyLength) + '...' : bodyStr
     }
 }
-
 
 module.exports = {
     getRoute: getRoute,
@@ -93,4 +89,4 @@ module.exports = {
     cleanOmitKeys: cleanOmitKeys,
     getLogLevel: getLogLevel,
     getBodyStr: getBodyStr
-};
+}
