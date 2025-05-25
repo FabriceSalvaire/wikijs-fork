714eec108fc29293ea452d7c047be546613d22db
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2025-05-26 00:30:01 +0200

    task linter

--- tasks/linter.py
+++ tasks/linter.py
@@ -22,22 +22,24 @@ def format(ctx, path, ext='.js', check=True):
     ]
     if check:
         cmd.append('--check')
-    # print(join_cmd(cmd))
-    # cmd.append(str(path))
-    # subprocess.run(cmd)
-    for root, dirs, files in path.walk():
-        root = Path(root)
-        # protection
-        for _ in ('assets', 'node_modules'):
-            if _ in dirs:
-                dirs.remove(_)
-        for _ in files:
-            _ = root.joinpath(_)
-            if _.name in (
-                'eslint.config-orig.js',
-            ):
-                continue
-            if _.suffix == ext:
-                cmd_ = cmd + [str(_)]
-                print(join_cmd(cmd_))
-                subprocess.run(cmd_)
+    if path.is_file():
+        print(join_cmd(cmd))
+        cmd.append(str(path))
+        subprocess.run(cmd)
+    else:
+        for root, dirs, files in path.walk():
+            root = Path(root)
+            # protection
+            for _ in ('assets', 'node_modules'):
+                if _ in dirs:
+                    dirs.remove(_)
+            for _ in files:
+                _ = root.joinpath(_)
+                if _.name in (
+                    'eslint.config-orig.js',
+                ):
+                    continue
+                if _.suffix == ext:
+                    cmd_ = cmd + [str(_)]
+                    print(join_cmd(cmd_))
+                    subprocess.run(cmd_)
