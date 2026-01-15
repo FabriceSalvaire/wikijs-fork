eb1d60969a99e6171adf9a936382c92a0ecb84b0
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2026-01-15 20:54:15 +0100

    katex helper code cleanup

--- client/components/editor/common/katex.js
+++ client/components/editor/common/katex.js
@@ -1,40 +1,41 @@
 // Test if potential opening or closing delimieter
 // Assumes that there is a "$" at state.src[pos]
-function isValidDelim (state, pos) {
-  let prevChar
-  let nextChar
+function is_valid_delim (state, pos) {
+  let prev_char
+  let next_char
   let max = state.posMax
-  let canOpen = true
-  let canClose = true
+  let can_open = true
+  let can_close = true
 
-  prevChar = pos > 0 ? state.src.charCodeAt(pos - 1) : -1
-  nextChar = pos + 1 <= max ? state.src.charCodeAt(pos + 1) : -1
+  prev_char = pos > 0 ? state.src.charCodeAt(pos - 1) : -1
+  next_char = pos + 1 <= max ? state.src.charCodeAt(pos + 1) : -1
 
   // Check non-whitespace conditions for opening and closing, and
   // check that closing delimeter isn't followed by a number
-  if (prevChar === 0x20/* " " */ || prevChar === 0x09/* \t */ ||
-          (nextChar >= 0x30/* "0" */ && nextChar <= 0x39/* "9" */)) {
-    canClose = false
-  }
-  if (nextChar === 0x20/* " " */ || nextChar === 0x09/* \t */) {
-    canOpen = false
-  }
+  if (prev_char === 0x20/* " " */ || prev_char === 0x09/* \t */ ||
+    (next_char >= 0x30/* "0" */ && next_char <= 0x39/* "9" */))
+    can_close = false
+  if (next_char === 0x20/* " " */ || next_char === 0x09/* \t */)
+    can_open = false
 
   return {
-    canOpen: canOpen,
-    canClose: canClose
+    can_open: can_open,
+    can_close: can_close
   }
 }
 
 export default {
+  // ---------------------------------------------
   katexInline (state, silent) {
     let start, match, token, res, pos
 
-    if (state.src[state.pos] !== '$') { return false }
+    if (state.src[state.pos] !== '$')
+      return false
 
-    res = isValidDelim(state, state.pos)
-    if (!res.canOpen) {
-      if (!silent) { state.pending += '$' }
+    res = is_valid_delim(state, state.pos)
+    if (!res.can_open) {
+      if (!silent)
+        state.pending += '$'
       state.pos += 1
       return true
     }
@@ -49,31 +50,36 @@ export default {
       // Found potential $, look for escapes, pos will point to
       // first non escape when complete
       pos = match - 1
-      while (state.src[pos] === '\\') { pos -= 1 }
+      while (state.src[pos] === '\\')
+        pos -= 1
 
       // Even number of escapes, potential closing delimiter found
-      if (((match - pos) % 2) === 1) { break }
+      if (((match - pos) % 2) === 1)
+        break
       match += 1
     }
 
     // No closing delimter found.  Consume $ and continue.
     if (match === -1) {
-      if (!silent) { state.pending += '$' }
+      if (!silent)
+        state.pending += '$'
       state.pos = start
       return true
     }
 
     // Check if we have empty content, ie: $$.  Do not parse.
     if (match - start === 0) {
-      if (!silent) { state.pending += '$$' }
+      if (!silent)
+        state.pending += '$$'
       state.pos = start + 1
       return true
     }
 
     // Check for valid closing delimiter
-    res = isValidDelim(state, match)
-    if (!res.canClose) {
-      if (!silent) { state.pending += '$' }
+    res = is_valid_delim(state, match)
+    if (!res.can_close) {
+      if (!silent)
+        state.pending += '$'
       state.pos = start
       return true
     }
@@ -85,8 +91,7 @@ export default {
         // Extract the math part without the $
         .slice(start, match)
         // Escape the curly braces since they will be interpreted as
-        // attributes by markdown-it-attrs (the "curly_attributes"
-        // core rule)
+        // attributes by markdown-it-attrs (the "curly_attributes" core rule)
         .replaceAll("{", "{{")
         .replaceAll("}", "}}")
     }
@@ -95,28 +100,33 @@ export default {
     return true
   },
 
+  // ---------------------------------------------
   katexBlock (state, start, end, silent) {
-    let firstLine; let lastLine; let next; let lastPos; let found = false; let token
+    let first_line; let last_line; let next; let last_pos; let found = false; let token
     let pos = state.bMarks[start] + state.tShift[start]
     let max = state.eMarks[start]
 
-    if (pos + 2 > max) { return false }
-    if (state.src.slice(pos, pos + 2) !== '$$') { return false }
+    if (pos + 2 > max)
+      return false
+    if (state.src.slice(pos, pos + 2) !== '$$')
+      return false
 
     pos += 2
-    firstLine = state.src.slice(pos, max)
+    first_line = state.src.slice(pos, max)
 
-    if (silent) { return true }
-    if (firstLine.trim().slice(-2) === '$$') {
+    if (silent)
+      return true
+    if (first_line.trim().slice(-2) === '$$') {
       // Single line expression
-      firstLine = firstLine.trim().slice(0, -2)
+      first_line = first_line.trim().slice(0, -2)
       found = true
     }
 
     for (next = start; !found;) {
       next++
 
-      if (next >= end) { break }
+      if (next >= end)
+        break
 
       pos = state.bMarks[next] + state.tShift[next]
       max = state.eMarks[next]
@@ -127,8 +137,8 @@ export default {
       }
 
       if (state.src.slice(pos, max).trim().slice(-2) === '$$') {
-        lastPos = state.src.slice(0, max).lastIndexOf('$$')
-        lastLine = state.src.slice(pos, lastPos)
+        last_pos = state.src.slice(0, max).lastIndexOf('$$')
+        last_line = state.src.slice(pos, last_pos)
         found = true
       }
     }
@@ -137,9 +147,9 @@ export default {
 
     token = state.push('katex_block', 'math', 0)
     token.block = true
-    token.content = (firstLine && firstLine.trim() ? firstLine + '\n' : '') +
+    token.content = (first_line && first_line.trim() ? first_line + '\n' : '') +
     state.getLines(start + 1, next, state.tShift[start], true) +
-    (lastLine && lastLine.trim() ? lastLine : '')
+    (last_line && last_line.trim() ? last_line : '')
     token.map = [ start, state.line ]
     token.markup = '$$'
     return true
